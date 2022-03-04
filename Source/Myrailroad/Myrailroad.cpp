fe1788d208d70ca51c487847a81f0ebdfb8a7f14
Simon Corté <simoncours@outlook.fr>
2022-03-04 02:11:55 +0100

    Initial commit

---
+++ Source/Myrailroad/Myrailroad.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Myrailroad.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Myrailroad, "Myrailroad" );
---
+++ Source/Myrailroad/MyrailroadGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyrailroadGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYRAILROAD_API AMyrailroadGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
